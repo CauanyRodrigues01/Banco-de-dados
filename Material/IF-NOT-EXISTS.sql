2f1e676ddae020a60baf112311df2ad48f714a87
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2021-03-04 12:50:16 -0300

    Alter docs

--- Material/IF-NOT-EXISTS.sql
+++
@@ -1,8 +0,0 @@
-USE cadastro;
-CREATE TABLE IF NOT EXISTS cursos (
-	nome VARCHAR(30) NOT NULL UNIQUE,
-    descricao TEXT,
-    carga INT UNSIGNED,
-    totaulas INT,
-    ano YEAR DEFAULT '2021'
-) DEFAULT CHARSET = utf8;
